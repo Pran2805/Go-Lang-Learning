29a76d9ac445e1401f0cb9df215294612183efb8
Pran2805 <pranavshinde.as@gmail.com>
2025-07-18 14:53:09 +0530

    Conversion and way to handle error: Easy way

--- basic_syntax/main.go
+++ basic_syntax/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func variables() {
@@ -51,11 +53,30 @@ func userInput() {
 	fmt.Println("Thanks for your input:", input)
 }
 
+func conversion() {
+	fmt.Println("Welcome to Our Website")
+	fmt.Println("Please rate our website between 1 to 10")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks For rating: ", input)
+
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Added 1 to your rating:", numRating+1)
+	}
+}
+
 // if the first letter is captial then it is the public otherwise private
 const LoginToken string = "This is my variable token"
 
 func main() {
 
 	// variables()
-	userInput()
+	// userInput()
+	conversion()
 }
