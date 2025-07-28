5b552ce9a37fda75284420d68b2600e65f9f6f82
Pran2805 <pranavshinde.as@gmail.com>
2025-07-28 17:32:10 +0530

    Functions

---
+++ functions/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+	result := addition(2, 4)
+
+	fmt.Println(result)
+	answer, _ := addder(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+	fmt.Println(answer)
+}
+
+func addition(val1 int, val2 int) int { // need to pass the type of variable
+	return val1 + val2
+}
+
+func addder(values ...int) (int, string) {
+	total := 0
+	for _, val := range values {
+		total += val
+	}
+	return total, "We can return two and multiple value like this"
+}
