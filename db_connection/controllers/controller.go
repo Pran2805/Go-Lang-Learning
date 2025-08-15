e520a3343ea53f8c06f872aa1c23b343feb324f5
Pran2805 <pranavshinde.as@gmail.com>
2025-08-15 16:04:25 +0530

    Creating folders, connecting db and creating a helpers

---
+++ db_connection/controllers/controller.go
@@ -0,0 +1,118 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/pran2805/go-lang-learning/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const MONGODB_URL string = "mongodb://127.0.0.1:27017/"
+const dbName string = "go-learning"
+const collectionName string = "watchlist"
+
+// most important
+var collection *mongo.Collection
+
+// connect with mongodb
+func init() {
+	// client options
+	clientOption := options.Client().ApplyURI(MONGODB_URL)
+
+	// connection to mongodb
+	client, err := mongo.Connect(context.TODO(), clientOption)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Mongodb Connection Success")
+
+	collection = client.Database(dbName).Collection(collectionName)
+
+	// connection instance
+	fmt.Println("Connection Instance is ready")
+}
+
+// Mongodb helpers - keep in seperate file
+
+// insert 1 record
+func insertMovie(movie models.Netflix) {
+	insertData, err := collection.InsertOne(context.Background(), movie)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted Movie data in db", insertData)
+}
+
+// update 1 record
+func updateMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	result, err := collection.UpdateByID(context.Background(), filter, update)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Updated Result", result)
+}
+
+func deleteMovieById(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+
+	filter := bson.M{"_id": id}
+
+	result, err := collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Deleted Result", result)
+}
+
+func deleteAllMovies() int64 {
+	result, err := collection.DeleteMany(context.Background(), bson.D{{}})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Deleted Result", result)
+
+	return result.DeletedCount
+}
+
+// get all movies from database
+func getAllMovies() []primitive.M {
+	cursor, err := collection.Find(context.Background(), bson.M{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var movies []primitive.M
+
+	for cursor.Next(context.Background()) {
+		var movie bson.M
+		err := cursor.Decode(&movie)
+
+		if err == nil {
+			log.Fatal(err)
+		}
+		movies = append(movies, movie)
+	}
+
+	cursor.Close(context.Background())
+	return movies
+}
---
+++ db_connection/main.go
@@ -0,0 +1,5 @@
+package main
+
+// in root directory there will be only one go file, otherwise it will not run
+func main() {
+}
